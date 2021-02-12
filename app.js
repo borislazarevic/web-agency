589366673183d5c5ad1d0cf832e79c21ce2d6c45
Boris Lazarevic <boris1991@live.com>
2021-02-12 13:47:40 +0100

    Navigation and Hamburger Menu

---
+++ app.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    // Navigation Toggle
+    nav.classList.toggle("nav-active");
+
+    // Animation Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkToggle 0.5s ease forwards ${
+          index / 7 + 0.4
+        }s`;
+      }
+    });
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
